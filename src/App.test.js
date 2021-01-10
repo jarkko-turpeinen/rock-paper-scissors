79d136c6665580ed3cdfa57170f135fd92c63550
jarkko-turpeinen <jarkko.turpeinen@swtech-fi>
2021-01-10 18:09:14 +0200

    remove test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App'
-
-test('renders title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Rock Paper Scissors/i)
-  expect(linkElement).toBeInTheDocument()
-})
